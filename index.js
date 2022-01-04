fa2a2fbcd6731c73c6f0513060a38ed000e4e0ac
Roma Kokontsev <Romeo42@ukr.net>
2022-01-04 14:34:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 class CountdownTimer {
-  constructor({ selector, targetDate}) {
+  constructor({ selector, targetDate }) {
     this.intervalId = null;
     this.refs = {
-  timerFace: document.querySelector(selector),
+      timerFace: document.querySelector(selector),
     };
-    this.valueRefs = this.refs.timerFace.querySelectorAll('.value');
+    this.valueRefs = this.refs.timerFace.querySelectorAll(".value");
     this.targetDate = targetDate;
   }
-  
-    intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = this.targetDate - currentTime;
-      const time = this.getTimeComponents(deltaTime);
 
-      this.updateClockFace(time);
-      this.finishTimer(time)
-    }, 1000)
-  
+  intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = this.targetDate - currentTime;
+    const time = this.getTimeComponents(deltaTime);
+
+    this.updateClockFace(time);
+    this.finishTimer(time);
+  }, 1000);
+
   finishTimer(time) {
-  if (time <= 0) {
-    clearInterval(this.intervalId);
-    this.refs.timerFace.textContent = `Конец`;
+    if (time <= 0) {
+      clearInterval(this.intervalId);
+      this.refs.timerFace.textContent = `Конец`;
+    }
   }
-  }
-  
-  updateClockFace(obj) {
-    this.valueRefs.forEach(el => el.textContent = obj[el.dataset.value])
-}
-  
-  pad(value) {
-  return String(value).padStart(2, '0');
-  }
-  
-  getTimeComponents(time) {
-  
-  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  return { days, hours, mins, secs }
-}
+  updateClockFace(obj) {
+    this.valueRefs.forEach((el) => (el.textContent = obj[el.dataset.value]));
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
 }
 
 new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('July 9, 2021'),
+  selector: "#timer-1",
+  targetDate: new Date("July 9, 2022"),
 });
